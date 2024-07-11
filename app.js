51ec68f86d198a21009735adc81a91e1b9250e00
unknown <agustinraggi@gmail.com>
2024-07-11 11:07:02 -0300

    se agrego mercado pago V 1.0

--- app.js
+++ app.js
@@ -1,6 +1,8 @@
 const express = require("express");
 const cors = require("cors");
 const mysql = require("mysql");
+const { MercadoPagoConfig, Preference } = require("mercadopago");
+
 const userRoutes = require("./user/createUser");
 const initDatabaseUser = require("./user/init-bd-user");
 const filmRoutes = require("./film/createFilm");
@@ -10,6 +12,11 @@ const app = express();
 
 app.use(cors());
 app.use(express.json());
+
+// Configuración de Mercado Pago
+const client = new MercadoPagoConfig({
+  accessToken: "TEST-8772233994679775-071021-e7cb928820036909642719afef88489f-309428726",
+});
 
 // Conexión principal a MySQL
 const db = mysql.createConnection({
@@ -52,8 +59,41 @@ db.connect((err) => {
                 return;
             }
             console.log("Conectado a la base de datos cine-aurora");
+
             app.use(userRoutes);
             app.use(filmRoutes);
+
+            app.post("/create_preference", async (req, res) => {
+                try {
+                    const body = {
+                        items: [
+                            {
+                                title: req.body.title,
+                                quantity: Number(req.body.quantity),
+                                unit_price: Number(req.body.price),
+                                currency_id: "ARS",
+                            },
+                        ],
+                        back_urls: {
+                            success: "https://www.youtube.com/@onthecode",
+                            failure: "https://www.youtube.com/@onthecode",
+                            pending: "https://www.youtube.com/@onthecode",
+                        },
+                        auto_return: "approved",
+                    };
+            
+                    const preference = new Preference(client);
+                    const result = await preference.create({ body });
+                    res.json({
+                        id: result.id,
+                    });
+                } catch (error) {
+                    console.error("Error al crear la preferencia en Mercado Pago:", error);
+                    res.status(500).json({
+                        error: "Error al crear la preferencia en Mercado Pago :(",
+                    });
+                }
+            });
 
             const PORT = process.env.PORT || 3001;
             app.listen(PORT, () => {
