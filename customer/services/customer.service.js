426f8668dd2c43d3a473268546ac22bce997ea77
unknown <agustinraggi@gmail.com>
2024-09-19 16:53:15 -0300

    Reemplazo del proyecto en main con el contenido de ORM

--- customer/services/customer.service.js
+++
@@ -1,9 +0,0 @@
-import {CustomerRepository} from '../repositories/custormer.repository.js'
-
-export class CustomerService{
-    async getById(idUser){
-        const customerRepository = new CustomerRepository();
-        const customer = await customerRepository.getById(idUser);
-        return customer;
-    }
-}
